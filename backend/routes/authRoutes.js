7954f111738ee9ea74f5f197ba8d0f1b959a8110
abhi <abhigowdabs154@gmail.com>
2023-12-21 19:53:08 +0530

    auth routes using controllers func , just to be clean code

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,18 +1,20 @@
 const express=require('express');
 const router=express.Router();
+const authControllers=require('../controllers/auth/authControllers')
 
 // Handle GET request to '/api/register' (for testing purposes)
 
-router.post('/register' ,(req , res)=>{
-    res.send('register route')
-});
+router.post('/register' ,authControllers.controllers.postRegister);
 
 // Handle GET request to '/api/login' (for testing purposes)
 
-router.post('/login',(req,res)=>{
+router.post('/login',authControllers.controllers.postLogin)
 
-    res.send(' login route')
-})
+
+// router.post('/login',(req,res)=>{
+
+//     res.send(' login route')
+// })
 
 module.exports=router;
 
